820a1d7bdaf7a648b3376c00397483ca960c5b4e
soferos <dominikszczepaniak97@gmail.com>
2024-05-30 02:09:59 +0100

    Finalised the project

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ def main():
     book_path = "books/frankenstein.txt"
     raw_text = get_book(book_path)
     num_words = count_words(raw_text)
+    print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
-    dictionary(raw_text)
+    count_and_report(raw_text)
+    print("--- End Report ---")
     
-
 def count_words(text):
     words = text.split()
     return len(words)
@@ -24,15 +25,24 @@ def get_book(path):
     with open("books/frankenstein.txt") as f:
         return f.read()
     
-def dictionary(text):
+def count_and_report(text):
     lowered_string = text.lower()
-    letters = []
-    for letter in lowered_string:
-        letters.append(letter)
-    letter_counts = dict(dict.fromkeys(letters))
-    for name in letter_counts:
-        
-        letter_counts[0] = count_letters(text, name)
-    print(letter_counts)
+    letter_dict = {letter:0 for letter in lowered_string if letter.isalpha()}
+    for letter in text:
+        if letter in letter_dict:
+            letter_dict[letter] += 1
+    dict_list = [{key: value} for key, value in letter_dict.items()]
+    dict_list.sort(reverse=True, key=sort_on)
+    for item in dict_list:
+        key = list(item.keys())[0]
+        value = list(item.values())[0]
+        if key.isalpha():
+            print(f"The '{key}' character was found {value} times.")
+        else:
+            return None
+
+def sort_on(d):
+    return list(d.values())[0]
+
 
 main()
